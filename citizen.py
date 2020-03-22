1b61d7d8049c1b01a765b2a7ae4bd8b8b69118f0
Nima Siboni <nima.siboni@gmail.com>
2020-03-22 21:27:31 +0100

    the class for citizens

---
+++ citizen.py
@@ -0,0 +1,12 @@
+class citizen:
+
+    # Class Attribute
+    info = 'human'
+
+    # Initializer / Instance Attributes
+    def __init__(self, pos, home, work, social_places, next_dest):
+        self.pos = pos
+        self.home = home
+        self.work = work
+        self.social_places = social_places
+        self.next_dest = next_dest
