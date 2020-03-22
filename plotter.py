0f03f00bf08374f039a47262530956f7638f5dda
Nima Siboni <nima.siboni@gmail.com>
2020-03-22 21:26:47 +0100

    a plotter which almost plots everything from simulations

---
+++ plotter.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotter(sth,smhow):
+    m = np.size(sth)
+    data = np.zeros((m,2))
+    for i in range(0,m):
+        data[i,:] = sth[i].pos
+    plt.plot(data[:,0],data[:,1],smhow)
+
