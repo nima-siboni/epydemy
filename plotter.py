00ec7dd93a88c30176c3e97bbf09d3001f04e333
Nima Siboni <nima.siboni@gmail.com>
2020-03-23 03:27:30 +0100

    plotter is made interactive

--- plotter.py
+++ plotter.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib import interactive
+interactive(True)
 def plotter(sth,smhow):
     m = np.size(sth)
     data = np.zeros((m,2))
     for i in range(0,m):
         data[i,:] = sth[i].pos
     plt.plot(data[:,0],data[:,1],smhow)
+
 
