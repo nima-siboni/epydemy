e5e2c3e444fd9aa9a0f8cee0ac013ee634790f47
Nima Siboni <nima.siboni@gmail.com>
2020-03-23 01:26:18 +0100

    a bug in volk[i].home = i is fixed to volk[pid].home = i; also a while loop is introduced to make sure that everyone is assigned to one building at least.

--- create_buildings.py
+++ create_buildings.py
@@ -14,7 +14,7 @@ def create_building(city_size, nr_people, nr_building):
         total_nr_assigned = total_nr_assigned + 1
         
     for i in range(0, nr_building):
-        nr_new_people_in_this_house = np.random.randint(1,high=5)
+        nr_new_people_in_this_house = np.random.randint(1, high = int(nr_people / nr_building))
         if (total_nr_assigned + nr_new_people_in_this_house <= nr_people ):
             upper_limit = total_nr_assigned + nr_new_people_in_this_house
         else:
@@ -48,26 +48,27 @@ def create_building_and_assign_volk(city_size, volk, nr_building, building_name)
             volk[i].home = i
         if (building_name=='work'):
             volk[i].work = i
-        
-    for i in range(0, nr_building):
-        nr_new_people_in_this_house = np.random.randint(1,high=5)
-        if (total_nr_assigned + nr_new_people_in_this_house <= nr_people ):
-            upper_limit = total_nr_assigned + nr_new_people_in_this_house
-        else:
-            upper_limit = nr_people
-                    
-        if (upper_limit_reached == False):
-            new_habitants = np.arange(total_nr_assigned, upper_limit)
-            buildings[i].peoples_id = np.concatenate((buildings[i].peoples_id, new_habitants))
-            for pid in range(total_nr_assigned, upper_limit):
-                if (building_name=='home'):
-                    volk[i].home = i
-                if (building_name=='work'):
-                    volk[i].work = i
-                
-        total_nr_assigned = upper_limit
-        
-        if (total_nr_assigned == nr_people):
-            upper_limit_reached = True
             
+    while (total_nr_assigned < nr_people):    
+        for i in range(0, nr_building):
+            nr_new_people_in_this_house = np.random.randint(1, high =  2*int(nr_people / nr_building)+1)
+            if (total_nr_assigned + nr_new_people_in_this_house <= nr_people ):
+                upper_limit = total_nr_assigned + nr_new_people_in_this_house
+            else:
+                upper_limit = nr_people
+                    
+            if (upper_limit_reached == False):
+                new_habitants = np.arange(total_nr_assigned, upper_limit)
+                buildings[i].peoples_id = np.concatenate((buildings[i].peoples_id, new_habitants))
+                for pid in range(total_nr_assigned, upper_limit):
+                    if (building_name=='home'):
+                        volk[pid].home = i
+                    if (building_name=='work'):
+                        volk[pid].work = i
+                
+            total_nr_assigned = upper_limit
+        
+            if (total_nr_assigned == nr_people):
+                upper_limit_reached = True
+        
     return buildings
